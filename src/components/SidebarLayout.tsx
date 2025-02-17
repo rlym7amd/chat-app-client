1e681e3929ff755eb6a744d1e17a32a6c58c03de
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-02-17 17:35:59 +0100

    create a peers component to prevent re-rendering

--- src/components/SidebarLayout.tsx
+++ src/components/SidebarLayout.tsx
@@ -20,10 +20,7 @@ interface Peer {
 export default function SidebarLayout() {
   const [user, setUser] = useState<User>();
   const [isUserLoading, setIsUserLoading] = useState(false);
-  const [peers, setPeers] = useState<Peer[]>();
-  const [isPeersLoading, setIsPeersLoading] = useState(false);
   const navigate = useNavigate();
-  const { conversationId } = useParams();
 
   useEffect(() => {
     setIsUserLoading(true);
@@ -34,20 +31,7 @@ export default function SidebarLayout() {
       .then((data) => setUser(data))
       .catch(() => navigate("login"))
       .finally(() => setIsUserLoading(false));
-
-    setIsPeersLoading(true);
-    fetchWithAuth(`${import.meta.env.VITE_API_DOMAIN}/api/participants/peers`, {
-      credentials: "include",
-    })
-      .then((res) => res.json())
-      .then((data) => setPeers(data.peers))
-      .catch(() => navigate("login"))
-      .finally(() => setIsPeersLoading(false));
-  }, [navigate]);
-
-  if (!peers) {
-    return <p>No converations</p>;
-  }
+  }, []);
 
   return (
     <div className="flex h-dvh text-neutral-900">
@@ -57,19 +41,7 @@ export default function SidebarLayout() {
           <SquarePen className="size-4" />
         </h2>
         <div className="mt-2 px-3 pt-2 h-full overflow-y-auto [scrollbar-width:thin] [scrollbar-color:rgb(220_220_220)_transparent]">
-          {isPeersLoading && <p>Loading...</p>}
-          {peers.map((peer) => (
-            <button
-              onClick={() => navigate(`/conversations/${peer.conversationId}`)}
-              className={`${
-                peer.conversationId === conversationId &&
-                "bg-neutral-200 hover:bg-neutral-200"
-              } w-full cursor-pointer hover:bg-neutral-100 transition-colors rounded-lg px-2 my-1 flex items-center gap-2`}
-            >
-              <MessageCircle className="size-4" />
-              <span className="px-2 py-1">{peer.user.name}</span>
-            </button>
-          ))}
+          <Peers />
         </div>
 
         <div className="mt-auto border-t border-neutral-400">
@@ -96,4 +68,43 @@ export default function SidebarLayout() {
       <Outlet />
     </div>
   );
+}
+
+function Peers() {
+  const [peers, setPeers] = useState<Peer[]>();
+  const [isPeersLoading, setIsPeersLoading] = useState(false);
+  const navigate = useNavigate();
+  const { conversationId } = useParams();
+
+  useEffect(() => {
+    setIsPeersLoading(true);
+
+    fetchWithAuth(`${import.meta.env.VITE_API_DOMAIN}/api/participants/peers`, {
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => setPeers(data.peers))
+      .finally(() => setIsPeersLoading(false));
+  }, []);
+
+  if (!peers) {
+    return <p>No peers</p>;
+  }
+
+  if (isPeersLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return peers.map((peer) => (
+    <button
+      onClick={() => navigate(`/conversations/${peer.conversationId}`)}
+      className={`${
+        peer.conversationId === conversationId &&
+        "bg-neutral-200 hover:bg-neutral-200"
+      } w-full cursor-pointer hover:bg-neutral-100 transition-colors rounded-lg px-2 my-1 flex items-center gap-2`}
+    >
+      <MessageCircle className="size-4" />
+      <span className="px-2 py-1">{peer.user.name}</span>
+    </button>
+  ));
 }

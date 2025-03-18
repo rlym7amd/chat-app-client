0b0bdc652e059bf2e00a014e64f418e8b1f0fe16
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-03-18 05:56:01 +0000

    use react hook form in add friend form

--- src/pages/AddFriend.tsx
+++ src/pages/AddFriend.tsx
@@ -1,54 +1,81 @@
-import { useState } from "react";
 import { fetchWithAuth } from "../utils/helpers";
-import { toast } from "react-toastify";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useNavigate } from "react-router";
+
+const addFriendSchema = z.object({
+  email: z
+    .string()
+    .nonempty("Email Address is required")
+    .email("Not a valid Email Address"),
+});
+
+type AddFriendInput = z.infer<typeof addFriendSchema>;
 
 export default function AddFriend() {
-  const [email, setEmail] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<AddFriendInput>({
+    resolver: zodResolver(addFriendSchema),
+  });
+  const navigate = useNavigate();
+  console.log(errors);
 
-  function SendRequest() {
-    setIsLoading(true);
-    fetchWithAuth(`${import.meta.env.VITE_API_DOMAIN}/api/friends`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email,
-      }),
-    })
-      .then((res) => {
-        if (!res.ok) {
-          return res.json().then((data) => {
-            throw new Error(data.message);
-          });
-        }
-        return res.json();
-      })
-      .then((data) => toast.success(data.message))
-      .catch((err) => toast.error(err.message))
-      .finally(() => setIsLoading(false));
-  }
+  const onSubmit: SubmitHandler<AddFriendInput> = async (data) => {
+    const res = await fetchWithAuth(
+      `${import.meta.env.VITE_API_DOMAIN}/api/friends`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: data.email,
+        }),
+      }
+    );
+
+    if (!res.ok) {
+      const error = await res.json();
+      setError("root", {
+        type: "validate",
+        message: error.message,
+      });
+      return;
+    }
+
+    navigate("/");
+  };
 
   return (
     <main className="p-4">
       <p className="text-base uppercase">Add Friend</p>
       <p className="text-neutral-700">You can add friends with their email</p>
-      <div className="mt-2 flex gap-2">
+      <p className="h-3 m-0 text-sm text-red-600 mt-1">
+        {errors.root
+          ? errors.root.message
+          : errors.email
+          ? errors.email.message
+          : ""}
+      </p>
+      <form onSubmit={handleSubmit(onSubmit)} className="mt-2 flex gap-2">
         <input
-          type="text"
+          type="email"
+          {...register("email")}
           placeholder="You can add friends with their email"
-          onChange={(e) => setEmail(e.target.value)}
           className="flex-1 p-3 border border-gray-300 rounded focus:outline-none focus:border-green-500 text-sm"
         />
         <button
-          onClick={SendRequest}
-          disabled={isLoading}
+          type="submit"
           className="bg-green-500 text-white text-sm font-medium transition-colors px-3 py-2 rounded cursor-pointer hover:bg-green-500/80"
         >
           Send Friend Request
         </button>
-      </div>
+      </form>
     </main>
   );
 }

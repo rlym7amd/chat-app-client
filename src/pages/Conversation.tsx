1500395b891bd5cfc624e83f60dfdb5a3d93b725
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-04-05 11:21:25 +0000

    Implement private chat rooms for conversations

--- src/pages/Conversation.tsx
+++ src/pages/Conversation.tsx
@@ -1,23 +1,19 @@
 import { format, parseISO } from "date-fns";
 import { useParams } from "react-router";
 import { useEffect, useState } from "react";
-import { User } from "../components/SidebarLayout";
 import { fetchWithAuth } from "../utils/helpers";
+import { io } from "socket.io-client";
+import { Conversation, Message } from "../utils/definitions";
+import { useAuth } from "../hooks/useAuth";
 
-interface Conversation {
-  id: string;
-  createdAt: string;
-  messages: {
-    content: string;
-    createdAt: string;
-    sender: User;
-  }[];
-}
+const socket = io("http://localhost:1337");
 
 export default function ConversationPane() {
   const { conversationId } = useParams();
   const [conversation, setConversation] = useState<Conversation>();
   const [isLoading, setIsLoading] = useState(false);
+  const [input, setInput] = useState("");
+  const { currentUser } = useAuth();
 
   useEffect(() => {
     setIsLoading(true);
@@ -28,6 +24,46 @@ export default function ConversationPane() {
       .then((data) => setConversation(data))
       .finally(() => setIsLoading(false));
   }, [conversationId]);
+
+  useEffect(() => {
+    // Join the conversation room
+    console.log(conversationId);
+    socket.emit("join conversation", conversationId);
+
+    socket.on("chat message", (msg: Message) => {
+      // Update the conversation state with the new message
+      setConversation((prevConversation) => {
+        if (prevConversation) {
+          return {
+            ...prevConversation,
+            messages: [...prevConversation.messages, msg],
+          };
+        }
+        return prevConversation;
+      });
+    });
+
+    return () => {
+      // Leave the conversation room
+      socket.emit("leave conversation", conversationId);
+      socket.off("chat message");
+    };
+  }, [conversationId]);
+
+  const sendMessage = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (input && currentUser) {
+      const newMessage: Message = {
+        content: input,
+        createdAt: new Date().toISOString(),
+        conversationId: conversationId || "",
+        sender: { ...currentUser },
+      };
+
+      socket.emit("chat message", newMessage);
+      setInput("");
+    }
+  };
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -52,16 +88,23 @@ export default function ConversationPane() {
         </div>
 
         {/* Input Area */}
-        <div className="flex gap-2">
-          <input
-            type="text"
-            placeholder="Type a message..."
-            className="flex-1 p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-black"
-          />
-          <button className="bg-black text-white font-medium transition-colors p-3 rounded-lg cursor-pointer hover:bg-black/90">
-            Send
-          </button>
-        </div>
+        <form onSubmit={sendMessage}>
+          <div className="flex gap-2">
+            <input
+              type="text"
+              placeholder="Type a message..."
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              className="flex-1 p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-black"
+            />
+            <button
+              type="submit"
+              className="bg-black text-white font-medium transition-colors p-3 rounded-lg cursor-pointer hover:bg-black/90"
+            >
+              Send
+            </button>
+          </div>
+        </form>
       </div>
     </main>
   );
